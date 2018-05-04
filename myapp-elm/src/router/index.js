5ea81769a4c85deafd13cfe43c843ffb5439e184
lanouhn <>
2018-05-04 22:30:12 +0800

    这是第一天

--- myapp-elm/src/router/index.js
+++ myapp-elm/src/router/index.js
@@ -1,15 +1,24 @@
+//获取组件
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
+import Login from '@/components/login'
 
 Vue.use(Router)
 
+//配置路由
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'HomePage',
       component: HomePage
+    },{
+
+      path: '/login',
+      name: 'login',
+      component: Login
+
     }
   ]
 })
