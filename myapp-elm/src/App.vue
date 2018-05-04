9589e7f35dc0b4db492a565dc18eea0d6e302bc3
lanouhn <>
2018-05-04 12:00:08 +0800

    22

--- myapp-elm/src/App.vue
+++ myapp-elm/src/App.vue
@@ -14,12 +14,13 @@ export default {
 </script>
 
 <style>
+  body,html{
+    width: 100%;
+    height: 100%;
+    font-size: 16px;
+  }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
