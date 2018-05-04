56db53228c85d543c1e1b1f4a8a6f26119facd81
wfr521 <1282930598@qq.com>
2018-05-04 11:54:46 +0800

    22

---
+++ myapp-elm/src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="heard-top">
+      <span class="wfr-heard-logo">ele.me</span>
+      <router-link to="/login">登录|注册</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomePage"
+    }
+</script>
+
+<style scoped>
+  .wfr-heard-logo{
+
+  }
+</style>
