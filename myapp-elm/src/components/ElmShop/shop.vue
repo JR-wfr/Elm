ef094577ca23aa6a3411bdb9ccf00042abf7e2bc
wfr521 <1282930598@qq.com>
2018-05-07 22:35:22 +0800

    22

--- myapp-elm/src/components/ElmShop/shop.vue
+++ myapp-elm/src/components/ElmShop/shop.vue
@@ -1,31 +1,123 @@
 <template>
   <div>
     <ul class="wfr-logo-location">
-      <li><i class="el-icon-search"></i></li>
-      <li>{{location}}</li>
-      <li>登录|注册</li>
+      <li>
+        <router-link to="/seach" class="wfr-logo-seach">
+          <i class="el-icon-search">
+          </i>
+        </router-link>
+      </li>
+      <router-link to="/">
+        <li class="wfr-icon-list">
+          {{location}}
+        </li>
+      </router-link>
+      <li class="wfr-icon-li">
+        <router-link to="/personal">
+          <svg viewBox="0 0 28 33" class="path2">
+            <path fill-rule="evenodd"
+                  d="M20.798 19.289c2.636-2.002 4.215-5.091 4.215-8.437 0-5.886-4.845-10.647-10.808-10.647S3.397 4.966 3.397 10.852c0 3.345 1.578 6.433 4.212 8.435l.264-2.678C4.358 18.32 1.591 21.403.168 25.187l1.478.556v-1.579c-1.485.73-1.485.73-1.501 1.079-.054.188-.054.188-.069.278a2.58 2.58 0 0 0-.026.229 9.112 9.112 0 0 0-.019.4c-.008.265-.014.617-.018 1.039-.005.511-.006 1.037-.006 1.451v.027c-.004 1.732 1.41 3.129 3.154 3.129h22.082a3.18 3.18 0 0 0 3.172-3.153l.011-1.305.008-.897.003-.296.001-.083v-.022-.006-.001l.002-.278-.093-.262c-1.385-3.918-4.203-7.122-7.812-8.88l.263 2.678zm-1.911-2.516l-2.045 1.553 2.309 1.125c2.856 1.392 5.106 3.949 6.218 7.093l-.09-.54V26.033l-.001.083-.003.296-.008.897-.011 1.305c0 .01-.011.021-.013.021H3.161c-.007 0 .005.011.005.032v-.031c0-.404.001-.92.006-1.418.004-.4.01-.732.017-.969.004-.121.008-.212.012-.262l-.006.043c-.009.06-.009.06-.058.229-.01.336-.01.336-1.49 1.063H2.74l.385-1.024c1.141-3.035 3.35-5.495 6.131-6.849l2.309-1.124-2.045-1.554c-1.859-1.412-2.964-3.576-2.964-5.92 0-4.129 3.418-7.488 7.649-7.488s7.649 3.359 7.649 7.488c0 2.344-1.106 4.509-2.966 5.921z"
+                  class="path1"></path>
+          </svg>
+        </router-link>
+      </li>
     </ul>
+    <swiper ref="mySwiper" :options="swiperOption" class="wfr-lunbo">
+      <!-- slides -->
+      <swiper-slide class="wfr-lunbo1">
+        <ul>
+          <li v-for="">
+
+          </li>
+        </ul>
+      </swiper-slide>
+
+      <swiper-slide class="wfr-lunbo2">
+        I'm Slide 2
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
+  import Vue from 'vue'
+  import VueAwesomeSwiper from 'vue-awesome-swiper'
+  Vue.use(VueAwesomeSwiper)
+  import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+  var api5 = "http://cangdu.org:8001/v2/index_entry";
   export default {
     name: "shop",
-    data(){
+    data() {
       return {
-        location:"",
+        location: "",
+        imgdata: null,
+        swiperOption:{
+          notNextTick: true,
+          loop:true
+        }
       }
     },
-    created() {
-      this.location = this.$route.query.name
+      created(){
+        this.location = this.$route.query.name
+        this.axios.get(api5).then((respoent) => {
+          this.imgdata = respoent.data
+        })
+      },
+    components: {
+      swiper,
+      swiperSlide
     }
   }
 </script>
 
 <style scoped>
-.wfr-logo-location {
-  display: flex;
-  justify-content: space-around;
-  justify-items: center;
-}
+  .wfr-logo-location {
+    display: flex;
+    justify-content: space-around;
+    background-color: #3190e8;
+    width: 100%;
+    height: 1.95rem;
+    line-height: 1.95rem;
+  }
+
+  .wfr-logo-seach {
+    color: #fff;
+    font-size: 1rem;
+  }
+
+  .wfr-icon-list {
+    width: 8rem;
+    color: #fff;
+    font-weight: 200;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  .wfr-icon-li {
+    color: #fff;
+  }
+
+  .path1 {
+    color: #fff;
+  }
+
+  .path2 {
+    fill: #fff;
+    margin-top: 0.4rem;
+    margin-left: 0.4rem;
+    width: 1.8rem;
+    height: 1.1rem;
+    line-height: 1.95rem;
+  }
+  .wfr-lunbo {
+    width: 100%;
+  }
+  .wfr-lunbo1{
+    width: 100%;
+  }
+  .wfr-lunbo2{
+    width: 100%;
+  }
 </style>
