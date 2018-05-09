572f81d0736b37bac7c2dd31e9bf5d3f62638193
wfr521 <1282930598@qq.com>
2018-05-09 08:33:22 +0800

    22

--- myapp-elm/src/main.js
+++ myapp-elm/src/main.js
@@ -14,10 +14,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
 
-// Vue.config.productionTip = false
-
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-Vue.use(VueAwesomeSwiper)
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
